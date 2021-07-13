b2912f383a250024e13658f30220fc1777edfbdf
EssienNsikak <nsikak.essienceo@gmail.com>
2021-07-13 16:55:05 +0100

    admin controller implemented

--- api/index.js
+++ api/index.js
@@ -12,28 +12,28 @@ app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
 
 app.use(paginate.middleware(
-    process.env.LIMIT, 
-    process.env.MAX_LIMIT
+  process.env.LIMIT,
+  process.env.MAX_LIMIT
 ));
 
 const mongoDB = process.env.MONGO_URI;
 const port = process.env.PORT;
 
 const runApp = async () => {
-    try {
-        await connect(mongoDB, {
-            useFindAndModify: false,
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-            useCreateIndex: true,
-        });
-        console.log(`App is successfully connected to ${mongoDB}`);
-        app.listen(port, () => {
-            console.log(`App is running on PORT: ${port}`);
-        })
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    await connect(mongoDB, {
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+    console.log(`App is successfully connected to ${mongoDB}`);
+    app.listen(port, () => {
+      console.log(`App is running on PORT: ${port}`);
+    })
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 runApp();
